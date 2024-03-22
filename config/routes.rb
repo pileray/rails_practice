d585207a04a89b7258626b3d65c64abde080e6c0
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-03-22 18:43:15 +0900

    fix turbo & fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :questions
+  root to: 'questions#index'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete 'logout', to: 'sessions#destroy'
